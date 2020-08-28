d7855780e441022cc3d169006ef69e3ab8ec7198
Xavier Ritch <xritch89@hotmail.com>
2020-08-28 18:35:51 -0400

    main form mounted

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App2 from './App2';
 
 const FEATURES = {
   Processor: [
@@ -46,4 +46,4 @@ const FEATURES = {
   ]
 };
 
-ReactDOM.render(<App features={FEATURES}/>, document.getElementById('root'));
+ReactDOM.render(<App2 features={FEATURES}/>, document.getElementById('root'));
