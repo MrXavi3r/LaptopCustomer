f03790c04d159d60a02efb9cffd0ef4004cbbc15
Xavier Ritch <xritch89@hotmail.com>
2020-08-28 21:25:37 -0400

    Updates

---
+++ precache-manifest.ec58189d401202b2cced6b91d68cdb94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416e7b298c557ca9ee46575c2cbd340c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2fb8a9ddc84c56709e2",
+    "url": "/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "54b4b7df634fcf9ded69",
+    "url": "/static/js/2.5397eb6e.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.5397eb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2fb8a9ddc84c56709e2",
+    "url": "/static/js/main.72c6620c.chunk.js"
+  },
+  {
+    "revision": "b4c61285d3cbcd8d7f6e",
+    "url": "/static/js/runtime-main.4df8d4ab.js"
+  }
+]);
